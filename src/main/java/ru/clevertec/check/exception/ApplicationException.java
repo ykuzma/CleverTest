cfa461d92e193d81310397e1e518df420445f012
Yury Kuzma <vterkin1388@gmail.com>
2024-07-09 17:24:08 +0300

    change gitignore

---
+++ src/main/java/ru/clevertec/check/exception/ApplicationException.java
@@ -0,0 +1,8 @@
+package main.java.ru.clevertec.check.exception;
+
+public class ApplicationException extends RuntimeException{
+
+    public ApplicationException(String message) {
+        super(message);
+    }
+}
