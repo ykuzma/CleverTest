b36122460175702668556ef1f1b533d61959ac7d
Yury Kuzma <vterkin1388@gmail.com>
2024-07-10 18:59:11 +0300

    fix bug

--- src/main/java/ru/clevertec/check/configuration/ConfigurationApp.java
+++ src/main/java/ru/clevertec/check/configuration/ConfigurationApp.java
@@ -3,12 +3,12 @@ package main.java.ru.clevertec.check.configuration;
 import java.io.PrintStream;
 
 public class ConfigurationApp {
-    private static int wholesale = 10;
 
-
-    private String pathProduct = "./src/main/resourses/products.csv";
-    private String pathDiscountCard = "./src/main/resourses/discountCards.csv";
+    private String pathProduct = "./src/main/resources/products.csv";
+    private String pathDiscountCard = "./src/main/resources/discountCards.csv";
     private String delimiter = ";";
+    private String savoTo = "./result.csv";
+    private static int wholesale = 10;
 
     private static ConfigurationApp configurationApp;
 
@@ -20,10 +20,29 @@ public class ConfigurationApp {
     }
 
 
+    public void setPathProduct(String pathProduct) {
+        this.pathProduct = pathProduct;
+    }
+
+    public void setPathDiscountCard(String pathDiscountCard) {
+        this.pathDiscountCard = pathDiscountCard;
+    }
+
+    public void setDelimiter(String delimiter) {
+        this.delimiter = delimiter;
+    }
+
+    public void setSavoTo(String savoTo) {
+        this.savoTo = savoTo;
+    }
+
+    public static void setWholesale(int wholesale) {
+        ConfigurationApp.wholesale = wholesale;
+    }
 
     private ConfigurationApp(){}
 
-    public static int getWholesale() {
+    public int getWholesale() {
         return wholesale;
     }
 
